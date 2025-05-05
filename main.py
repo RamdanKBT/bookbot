0d0eb8cb38a7a16904899ebb0cec2dc88d818a66
RamdanKBT <ramdankatakpawou@gmail.com>
2025-05-05 23:36:50 +0200

    add function sort_characters_count to print a "pretty" report

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from stats import get_num_words
 from stats import count_characters
+from stats import sort_characters_counts
 
 def get_book_text(path_to_file):
     with open(path_to_file, "r" , encoding="utf-8") as f:
        text = f.read()
-    return text    
+    return text
 
 
 def main():
@@ -12,8 +13,14 @@ def main():
     book_text = get_book_text(path_to_file)
     words_in_text = get_num_words(book_text)
     letter_count_in_text = count_characters(book_text)
-    print(book_text)
-    print(words_in_text)
-    print(letter_count_in_text)
-
+    sorted_character_list = sort_characters_counts(letter_count_in_text)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_file}")
+    print("----------- Word Count ----------")
+    print(f"Found {words_in_text} total words")
+    print("--------- Character Count -------")
+    for item in sorted_character_list:
+        if item["char"].isalpha():
+            print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -1,9 +1,10 @@
+import operator
 def get_num_words(book_text):
      words = book_text.split()
      num_words = 0
      for word in words:
         num_words += 1
-     return f"{num_words} words found in the document"
+     return num_words
 
 def count_characters(text):
     char_counts = {}
@@ -11,3 +12,11 @@ def count_characters(text):
         lower_char = char.lower()
         char_counts[lower_char] = char_counts.get(lower_char, 0) + 1
     return char_counts
+
+def sort_characters_counts(char_counts):
+    sorted_counts_list = []
+    for char, count in char_counts.items():
+        if char.isalpha():
+            sorted_counts_list.append({"char": char, "num": count})
+    sorted_counts_list.sort(key=lambda item: item['num'], reverse=True)
+    return sorted_counts_list
